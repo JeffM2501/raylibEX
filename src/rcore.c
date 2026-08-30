1fb874b981c9aa954812bd3cdffb37dd90116604
Jeffery Myers <jeffm2501@gmail.com>
2026-08-30 11:51:58 -0700

    make screenshot key optional at runtime and consistent with exit key

--- src/rcore.c
+++ src/rcore.c
@@ -333,6 +333,7 @@ typedef struct CoreData {
     struct {
         struct {
             int exitKey;                    // Default exit key
+            int screenshotKey;              // Default screenshot key
             char currentKeyState[MAX_KEYBOARD_KEYS]; // Registers current frame key state
             char previousKeyState[MAX_KEYBOARD_KEYS]; // Registers previous frame key state
 
@@ -657,6 +658,7 @@ void InitWindow(int width, int height, const char *title)
     // Initialize global input state
     memset(&CORE.Input, 0, sizeof(CORE.Input)); // Reset CORE.Input structure to 0
     CORE.Input.Keyboard.exitKey = KEY_ESCAPE;
+    CORE.Input.Keyboard.screenshotKey = KEY_F12;
     CORE.Input.Mouse.scale = (Vector2){ 1.0f, 1.0f };
     CORE.Input.Mouse.cursor = MOUSE_CURSOR_ARROW;
     CORE.Input.Gamepad.lastButtonPressed = GAMEPAD_BUTTON_UNKNOWN;
@@ -944,7 +946,7 @@ void EndDrawing(void)
 #endif
 
 #if SUPPORT_SCREEN_CAPTURE
-    if (IsKeyPressed(KEY_F12))
+    if (IsKeyPressed(CORE.Input.Keyboard.screenshotKey))
     {
         TakeScreenshot(TextFormat("screenshot%03i.png", screenshotCounter));
         screenshotCounter++;
@@ -3995,6 +3997,11 @@ int GetCharPressed(void)
 void SetExitKey(int key)
 {
     CORE.Input.Keyboard.exitKey = key;
+}
+
+void SetScreenshotKey(int key)
+{
+    CORE.Input.Keyboard.screenshotKey = key;
 }
 
 //----------------------------------------------------------------------------------
